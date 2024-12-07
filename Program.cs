1984d296b1d17031684092b1b37957e0673114ff
Ali <126514615+aekC9@users.noreply.github.com>
2024-12-07 11:10:41 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ForLoopDersi;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please intput a value: ");
+        int n = int.Parse(Console.ReadLine());
+        for(int i = 0; i <= n; i++)
+        {
+            string spaces = new string(' ',n - i);
+            string hashes = new string('#',i);
+            Console.WriteLine(spaces+hashes);
+        }
+    }
+}
